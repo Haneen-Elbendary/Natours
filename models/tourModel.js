b2ecfa2588bf18a76a863ac7670acbeb94071551
Haneen <haneenelbendary22022@gmail.com>
2025-01-08 19:40:41 +0200

    modelling-locations-geospatial-data

--- models/tourModel.js
+++ models/tourModel.js
@@ -92,7 +92,27 @@ const tourSchema = new mongoose.Schema(
     secretTour: {
       type: Boolean,
       default: false
-    }
+    },
+    // GeoJson
+    startLocation: {
+      type: {
+        type: String,
+        default: 'Point',
+        enum: ['Point']
+      },
+      coordinates: [Number],
+      address: String,
+      description: String
+    },
+    locations: [
+      {
+        type: { type: String, default: 'Point', enum: ['Point'] },
+        coordinates: [Number],
+        address: String,
+        description: String,
+        day: Number
+      }
+    ]
   },
   {
     toJSON: { virtuals: true },

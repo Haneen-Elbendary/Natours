ed44f862e5d118b00933cfcc9f1ae165b680ade0
Haneen <haneenelbendary22022@gmail.com>
2025-02-07 04:27:02 +0200

    update user data with our API second way | updating user password with our API

--- public/js/index.js
+++ public/js/index.js
@@ -2,10 +2,14 @@
 import '@babel/polyfill';
 import { login, logout } from './login';
 import { showMap } from './mapbox';
+import { updateSettings } from './updateSettings';
 // DOM element
 const mapBox = document.getElementById('map');
 const loginForm = document.querySelector('.form--login');
 const logoutBtn = document.querySelector('.nav__el--logout');
+const formUserData = document.querySelector('.form-user-data');
+const formUserPassword = document.querySelector('.form-user-password');
+const btnPassword = document.querySelector('.btn-password');
 // Values
 // Delegation
 if (mapBox) {
@@ -21,3 +25,28 @@ if (loginForm) {
   });
 }
 logoutBtn.addEventListener('click', logout);
+if (formUserData) {
+  formUserData.addEventListener('submit', e => {
+    e.preventDefault();
+    const email = document.getElementById('email').value;
+    const name = document.getElementById('name').value;
+    updateSettings({ name, email }, 'data');
+  });
+}
+if (formUserPassword) {
+  formUserPassword.addEventListener('submit', async e => {
+    e.preventDefault();
+    btnPassword.innerHTML = 'Updating...';
+    const passwordCurrent = document.getElementById('password-current').value;
+    const password = document.getElementById('password').value;
+    const passwordConfirm = document.getElementById('password-confirm').value;
+    await updateSettings(
+      { passwordCurrent, password, passwordConfirm },
+      'password'
+    );
+    btnPassword.innerHTML = 'SAVE PASSWORD';
+    document.getElementById('password-current').textContent = ' ';
+    document.getElementById('password').textContent = ' ';
+    document.getElementById('password-confirm').textContent = ' ';
+  });
+}

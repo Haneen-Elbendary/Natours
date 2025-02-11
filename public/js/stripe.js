471962476e6f3e7d84b84d5b60a97ca7438be839
Haneen <haneenelbendary22022@gmail.com>
2025-02-11 05:11:43 +0200

    finish payment with stripe | 😉

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -1,19 +1,28 @@
 /* eslint-disable */
 import axios from 'axios';
 import { showAlert } from './alerts';
-const stripe = Stripe(`${process.env.STRIPE_PUBLIC_KEY}`);
+import { loadStripe } from '@stripe/stripe-js';
+
+// const stripe = Stripe(`${process.env.STRIPE_PUBLIC_KEY}`);
 
 export const bookTour = async tourId => {
   try {
+    const stripe = await loadStripe(`${process.env.STRIPE_PUBLIC_KEY}`);
     // 1- get the checkout session from the back-end
     const session = await axios(
       `http://127.0.0.1:3000/api/v1/bookings/checkout-session/${tourId}`
     );
     console.log(session);
     // 2- create checkout form + charge the credit card
-    await stripe.redirectToCheckout({
-      sessionId: session.data.session.id
-    });
+    // Get the session URL
+    const checkoutUrl = session.data.session.url;
+
+    // Redirect the user to the Stripe checkout page
+    window.location.href = checkoutUrl;
+
+    // await stripe.redirectToCheckout({
+    //   sessionId: session.data.session.id
+    // });
   } catch (err) {
     console.log(err);
     showAlert('error', err);
